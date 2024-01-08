a447e27b61913e22d3eb0760b41369a4ba414437
G00402034 <G00402034@atu.ie>
2024-01-08 02:42:19 +0000

    Added AccountService to update and get the balance

--- src/main/java/ie/atu/account_service/Account.java
+++ src/main/java/ie/atu/account_service/Account.java
@@ -3,27 +3,29 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
-public class Account {
 
     @Entity
     @Data
     @NoArgsConstructor
+    @AllArgsConstructor
     public class Account {
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private long id;
-        private String accountNumber;
-        private String accountHolderName;
-        private long balance;
+        private String AccountNumber;
+        private String AccountHolderName;
+        private long Balance;
 
         public Account(String accountNumber, String accountHolderName, long balance) {
-            this.accountNumber = accountNumber;
-            this.accountHolderName = accountHolderName;
-            this.balance = balance;
+           AccountNumber = accountNumber;
+           AccountHolderName = accountHolderName;
+           Balance = balance;
         }
-}
+    }
+
